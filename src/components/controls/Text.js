d0dd701b61cd9e3be4173d8dfbf44db2048d643e
pengjoshua <joshuapeng7@gmail.com>
2017-04-27 21:44:35 -0700

    Add project files

---
+++ src/components/controls/Text.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Text extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: props.value
+    };
+  }
+
+  onChange(e) {
+    this.setState({value: e.target.value}, function() {
+      this.props.onChange(this.state.value);
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <input type="number" value={this.state.value} onChange={this.onChange.bind(this)} />
+      </div>
+    );
+  }
+}
+
+export default Text;
